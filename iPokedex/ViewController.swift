9524616d2043870e660e2c0e18c12d880f7ba1cf
Hurlarious <hurlarious@gmail.com>
2016-06-13 13:15:18 -0700

    Initial Commit

---
+++ iPokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPokedex
+//
+//  Created by Dave Hurley on 2016-06-13.
+//  Copyright © 2016 Dave Hurley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
