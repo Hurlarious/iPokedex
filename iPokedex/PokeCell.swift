946228d9d2bf6000f47fd327bcf914477d433136
Hurlarious <hurlarious@gmail.com>
2016-06-13 16:31:52 -0700

    collection view delegate/datasource

---
+++ iPokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  iPokedex
+//
+//  Created by Dave Hurley on 2016-06-13.
+//  Copyright © 2016 Dave Hurley. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        nameLabel.text = self.pokemon.name.capitalizedString
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
