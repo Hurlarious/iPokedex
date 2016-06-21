bee1cc3aac134550466fed6e1f225ffb586a96fe
Hurlarious <hurlarious@gmail.com>
2016-06-21 16:46:32 -0700

    detail vc storyboard

--- iPokedex/DetailViewController.swift
+++ iPokedex/DetailViewController.swift
@@ -10,33 +10,22 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    
-    
+    // MARK: - Variables/Properties/Outlets
+
     @IBOutlet weak var nameLabel: UILabel!
     var pokemon: Pokemon!
 
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name.capitalizedString
         
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
